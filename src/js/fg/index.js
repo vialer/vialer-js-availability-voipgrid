3516e36d7df348941d0cb1c5d8fa8b9acf9a381d
Jeroen van Veen <j.veenvan@gmail.com>
2018-08-30 10:51:38 +0200

    fix: dont use contructor name for component id, because of uglify-js.

--- src/js/fg/index.js
+++ src/js/fg/index.js
@@ -7,6 +7,8 @@ const Plugin = require('vialer-js/lib/plugin')
 class PluginAvailabilityVg extends Plugin {
     constructor(app) {
         super(app)
+
+        this.name = 'AvailabilityVg' // Needed to lookup the default component.
         this.app.components.AvailabilityVg = require('../../components/availability')
         this.app.components.AccountPickerInstructions = require('../../components/account_picker_instructions')
     }
